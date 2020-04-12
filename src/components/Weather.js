06e0eb06b2c52f234e4e59c2a095e209173e1e63
linxea <linxea.linxea@gmail.com>
2020-04-12 15:24:09 +0800

    Add weather placeholder

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,10 +2,17 @@ import React from "react";
 import style from "./Weather.module.css";
 
 const Weather = ({ weather }) => {
-  if (weather) {
-    return <h1 className={style.weather}>{weather}</h1>;
+  let weatherStyle = [style.weather];
+
+  if (!weather) {
+    weatherStyle = [...weatherStyle, style.placeholder];
   }
-  return null;
+
+  return (
+    <h1 className={weatherStyle.join(" ")}>
+      {weather ? weather : "Weather Forecast"}
+    </h1>
+  );
 };
 
 export default Weather;
